8d5432a74c47a290ef8ff77917a909d38ad91a49
Arman Ansari <arman.ansari@infobeans.com>
2024-01-04 15:00:13 +0530

    few other class is added as comment in view.py

--- GenericsProject/CBVapp/views.py
+++ GenericsProject/CBVapp/views.py
@@ -12,3 +12,38 @@ class CourseListView(generics.ListCreateAPIView):
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = Course.objects.all()
   serializer_class = CourseSerializer
+  
+  
+"""  
+class CourseListView(generics.ListAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer  
+
+class CourseListView(generics.CreateAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer  
+"""
+
+
+"""
+class CourseDetailView(generics.RetrieveAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+  
+class CourseDetailView(generics.UpdateAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+  
+class CourseDetailView(generics.DestroyAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+
+class CourseDetailView(generics.RetrieveUpdateAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+
+class CourseDetailView(generics.RetrieveDestroyAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+"""
+  
